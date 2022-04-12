1fc0cd04ce38a363abbca95aad3a112ef889db25
Matias <leitnermatias22@gmail.com>
2022-04-12 20:02:57 -0300

    Initial schema and migration for database

---
+++ backend/prisma/migrations/20220412225942_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `financialCapital` DECIMAL(65, 30) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Account` (
+    `account_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `userId` BIGINT NULL,
+
+    PRIMARY KEY (`account_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
