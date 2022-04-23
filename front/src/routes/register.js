fb9dade4317c06c18013a8c7767baf1b375805c3
Argi Katsigiannides <argiriskatse@gmail.com>
2022-04-24 00:27:38 +0300

    create basic register form

--- front/src/routes/register.js
+++ front/src/routes/register.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import '../styles/register.css'
 
 const RegisterPage = () => {
   return (
-    <h1>Sign up Page</h1>
+    <div className="login-box">
+      <h1>Register</h1>
+      <form>
+        <div className="user-box">
+          <input type="text" name="" required="" placeholder='Email'></input>
+        </div>
+        <div className="user-box">
+          <input type="password" name="" required="" placeholder='Password'></input>
+        </div>
+        <div className="user-box">
+          <input type="password" name="" required="" placeholder='Password'></input>
+        </div>
+        <button id="btn">
+            Sign up
+        </button>
+      </form>
+    </div>
   )
 }
 
