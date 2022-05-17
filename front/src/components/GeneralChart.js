d3c8a0e59434fdc2a2c7b4da83bd553edb8a9dc6
Ian <iandavid.dev@protonmail.com>
2022-05-16 23:10:16 -0300

    ChartJS configuration in progress, changes inside GeneralChart.js component

--- front/src/components/GeneralChart.js
+++ front/src/components/GeneralChart.js
@@ -2,33 +2,48 @@ import React from "react";
 // Styles
 import "../styles/component-styles/GeneralChart.css"
 // ChartJS
-import "chart.js"
-import { Line } from "react-chartjs-2"
+import { Chart as ChartJS,CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend } from "chart.js"
+import { Bar } from "react-chartjs-2"
+// Adapter
+import "chartjs-adapter-date-fns"
 
-const income = [62120, 71340, 84382, 98120, 104100]
-const labels = [1, 2, 3, 4, 5]
-const options = {
-    responsive: true,
-    backgroundColor: "#2B2B2B",
-    borderColor: "#E8BF59",
-    color: "white",
-};
+ChartJS.register(CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend);
+
+// User data
+const data = {
+    datasets: [{
+        label: "Monthly income",
+        data: [
+            {x:"2022-06-16", y: 42220},
+            {x:"2022-06-18", y: 5990},
+            {x:"2022-06-20", y: 70090},
+            {x:"2022-06-25", y: 84090}
+        ],
+        backgroundColor: "orange"
+    }]
+}
+
+// options of the chart
+const config = {
+    data,
+    options: {
+        scales: {
+            x: {
+                type: "time",
+                time: {
+                    unit: "day"
+                }
+            }
+        }
+    }
+}
+
 
 function GeneralChart() {
     return (
         <div className="generalChartContainer">
-        <Line
-            data={{
-                datasets: [
-                    {
-                        label: "Monthly income and expenses",
-                        data: income,
-                    }
-                ],
-                labels
-            }}
-            options={options}
-        />
+            <Bar data={{config}}
+            />
         </div>
     );
 }
