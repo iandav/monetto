7d60fde02480f6ac5344f3d88777de68bdac5113
Ian <iandavid.dev@protonmail.com>
2022-05-18 23:14:14 -0300

    Chart configuration edited, date adapter imported inside GeneralChart.js component

--- front/src/components/GeneralChart.js
+++ front/src/components/GeneralChart.js
@@ -1,81 +1,63 @@
 import React from "react";
 // Styles
 import "../styles/component-styles/GeneralChart.css"
-// ChartJS components
-import { Chart } from "react-chartjs-2"
 // ChartJS dependencies
+import { Chart } from "react-chartjs-2"
 import {
-    Chart as ChartJS,
-    ArcElement,
-    LineElement,
-    BarElement,
-    PointElement,
-    BarController,
-    BubbleController,
-    DoughnutController,
-    LineController,
-    PieController,
-    PolarAreaController,
-    RadarController,
-    ScatterController,
-    CategoryScale,
-    LinearScale,
-    LogarithmicScale,
-    RadialLinearScale,
-    TimeScale,
-    TimeSeriesScale,
-    Decimation,
-    Filler,
-    Legend,
-    Title,
-    Tooltip,
-    SubTitle
-  } from 'chart.js';
-  
-  ChartJS.register(
-    ArcElement,
-    LineElement,
-    BarElement,
-    PointElement,
-    BarController,
-    BubbleController,
-    DoughnutController,
-    LineController,
-    PieController,
-    PolarAreaController,
-    RadarController,
-    ScatterController,
-    CategoryScale,
-    LinearScale,
-    LogarithmicScale,
-    RadialLinearScale,
-    TimeScale,
-    TimeSeriesScale,
-    Decimation,
-    Filler,
-    Legend,
-    Title,
-    Tooltip,
-    SubTitle
-  );
-
-// Chart configuration (ignore the following variables)
-const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
-
-const data = {
-    labels: labels,
-    datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30],
-    }]
-  };
-
-const config = {
-    data: data,
-    options: {}
-  };
+  Chart as ChartJS,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+// ChartJS date adapter
+import "chartjs-adapter-date-fns"
+ChartJS.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
 
 
 function GeneralChart() {
@@ -101,8 +83,10 @@ function GeneralChart() {
                     }]
                 }}
                 options={{
+                    responsive: true,
                     
                 }}
+                
               />
             </div>
         </>
