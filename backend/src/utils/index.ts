6cbe3fdd56d9e7ffe31781cfb67df3c955f56b86
Matias <leitnermatias22@gmail.com>
2022-05-22 10:26:51 -0300

    Refactor utils dayjs

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,7 +1,8 @@
-import dayjs from "dayjs";
+import {default as time} from "dayjs";
 import utc from "dayjs/plugin/utc";
 
-// By default accept UTC as the only timezone for everything
-dayjs.extend(utc)
 
-exports.dayjs = dayjs;
+// By default accept UTC as the only timezone for everything
+time.extend(utc)
+
+export const dayjs = time;
