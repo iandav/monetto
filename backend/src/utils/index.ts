e8ff62713dcdf805ed17b2b22c8c21177cc3039f
Matias <leitnermatias22@gmail.com>
2022-05-22 10:06:55 -0300

    Refactor index.ts in utils

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,5 +1,5 @@
-const dayjs = require("dayjs")
-const utc = require("dayjs/plugin/utc")
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
 // By default accept UTC as the only timezone for everything
 dayjs.extend(utc)
