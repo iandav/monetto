d2b6c0479c92f73490fd52e91a18e9bd3651d127
Matias <leitnermatias22@gmail.com>
2022-05-22 09:59:28 -0300

    Refactor earning routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { authRouter } from "./auth.routes";
 import {accountRouter} from "./account.routes";
-import earningRoutes from "./earning.routes";
+import {earningRouter} from "./earning.routes";
 import expenseRoutes from "./expense.routes";
 import {userRouter} from "./user.routes";
 import { Router } from "express";
@@ -9,7 +9,7 @@ const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/account", accountRouter);
-// router.use("/earning", earningRoutes);
+router.use("/earning", earningRouter);
 // router.use("/expense", expenseRoutes);
 router.use("/user", userRouter);
 
