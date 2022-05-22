3103116c6e26ac5ee351c82bc0f8ae8debc1f557
Matias <leitnermatias22@gmail.com>
2022-05-22 17:02:29 -0300

    Add morgan logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@
 import express from "express";
 import cors from "cors";
 import cookieSession from "cookie-session";
+import morgan from "morgan";
 import mainRouter from "./routes"
 
 // Server configurations
@@ -21,7 +22,8 @@ app.use(cookieSession({
     name: "monetto-session",
     secret: "SECRET", // should be changed to env variable
     httpOnly: true
-}))
+}));
+app.use(morgan('combined'));
 
 app.use("/api", mainRouter);
 
