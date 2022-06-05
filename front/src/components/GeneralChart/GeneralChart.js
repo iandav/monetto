eb2d5cf8884844b978262655149980637834b03b
Argi Katsigiannides <argiriskatse@gmail.com>
2022-06-05 15:39:46 +0300

    fix the weird semicolon at the async inside useEffect in the GeneralChart.js

--- front/src/components/GeneralChart/GeneralChart.js
+++ front/src/components/GeneralChart/GeneralChart.js
@@ -61,12 +61,14 @@ ChartJS.register(
 function GeneralChart() {
   const [earnings, setEarnings] = useState([])
 
-  useEffect(() => {
-    ;(async () => {
-      const data = await getEarningsFromUser()
-      setEarnings(data)
-    })()
-  }, [])
+  useEffect(
+    () =>
+      (async () => {
+        const data = await getEarningsFromUser()
+        setEarnings(data)
+      })(),
+    []
+  )
 
   return (
     <Chart
